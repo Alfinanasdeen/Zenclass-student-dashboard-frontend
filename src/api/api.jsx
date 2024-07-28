310180d77280c1dc36058f962cade38b50653ae9
Alfinanasdeen <alfinanasdeen@gmail.com>
2024-07-28 07:52:39 +0530

    commit

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,8 +1,24 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const api = axios.create({
   baseURL: "http://localhost:3000",
   withCredentials: true,
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.data.message === "TokenExpired") {
+      // Handle token expiration
+      localStorage.removeItem("token");
+      localStorage.removeItem("userData");
+      // Use navigate function to redirect
+      const navigate = useNavigate();
+      navigate("/login");
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;
