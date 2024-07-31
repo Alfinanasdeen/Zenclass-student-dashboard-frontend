f2213fa36b6f98fe35b89595eaac7ce1ecf32c10
Alfinanasdeen <alfinanasdeen@gmail.com>
2024-07-31 22:25:30 +0530

    commit

--- src/pages/Login/login.jsx
+++ src/pages/Login/login.jsx
@@ -17,72 +17,61 @@ const Login = () => {
     password: Yup.string().required("Required"),
   });
 
-
   return (
     <div className="loginPage">
-      <div className="row m-0">
-        <div className="col-md-8">
-          <div className="row img__container">
-            <img src={LOGO} alt="Logo" className="logo" />
-          </div>
-          <div className="row  d-flex justify-content-center align-items-center">
-            <div className="col-10 col-md-8 col-lg-6 top-area">
-              <Formik
-                initialValues={{ email: "", password: "" }}
-                validationSchema={validationSchema}
-                onSubmit={(values) => handleSignIn(values)}
-              >
-                {() => (
-                  <Form>
-                    <TextField
-                      label="Email"
-                      name="email"
-                      type="email"
-                      placeholder="Example: johndoe@mail.com"
-                    />
-                    <TextField
-                      label="Password"
-                      name="password"
-                      type="password"
-                      placeholder="Your Password"
-                    />
-                    <button
-                      type="submit"
-                      className="col-md-12 login__btn mt-4 mb-4"
-                    >
-                      {isLoading ? (
-                        <span className="spinner-border text-warning"></span>
-                      ) : (
-                        "Login"
-                      )}
-                    </button>
-                  </Form>
-                )}
-              </Formik>
-              <Link
-                to="/forgot"
-                className="col-md-12 btn custom-btn custom-btn-outline-danger"
-              >
-                Forgot Password?
-              </Link>
-              <Link
-                to="/signup"
-                className="col-md-12 btn custom-btn custom-btn-outline-success"
-              >
-                Not Registered? Sign up
-              </Link>
-              <button
-                className="col-md-12 btn custom-btn custom-btn-outline-success"
-                data-bs-toggle="modal"
-                data-bs-target="#demoModal"
-              >
-                Demo Login
-              </button>
+      <div className="">
+        <div className="">
+          <div className="login-container">
+            <div className="logo__container">
+              <img src={LOGO} alt="Logo" className="logo" />
+            </div>
+            <Formik
+              initialValues={{ email: "", password: "" }}
+              validationSchema={validationSchema}
+              onSubmit={(values) => handleSignIn(values)}
+            >
+              {() => (
+                <Form>
+                  <TextField
+                    label="Email"
+                    name="email"
+                    type="email"
+                    placeholder="Example: johndoe@mail.com"
+                  />
+                  <TextField
+                    label="Password"
+                    name="password"
+                    type="password"
+                    placeholder="Your Password"
+                  />
+                  <Link to="/forgot" className="link">
+                    Forgot Password?
+                  </Link>
+                  <button type="submit" className="login-btn">
+                    {isLoading ? (
+                      <span className="spinner-border text-warning"></span>
+                    ) : (
+                      "Login"
+                    )}
+                  </button>
+                  <Link to="/signup" className="link">
+                    Not Registered? Sign up
+                  </Link>
+                </Form>
+              )}
+            </Formik>
+
+            {/* <button
+              className=""
+              data-bs-toggle="modal"
+              data-bs-target="#demoModal"
+            >
+              Demo Login
+            </button> */}
+            <div className="banner-container">
+              <img src={BANNER} className="banner" alt="Banner" />
             </div>
           </div>
-        </div>
-        <div className="col-md-4 banner__right px-0">
-          <img src={BANNER} className="banner" alt="Banner" />
         </div>
       </div>
 
@@ -93,7 +82,7 @@ const Login = () => {
               <h4 className="modal-title">Welcome to Zen Student Dashboard</h4>
               <button
                 type="button"
-                className="btn-close"
+                className=""
                 data-bs-dismiss="modal"
               ></button>
             </div>
@@ -103,7 +92,7 @@ const Login = () => {
                 <div>Email: student@gmail.com</div>
                 <div>Password: student@123</div>
                 <hr />
-                <p className="text-secondary">
+                <p className="">
                   Note: The Student Dashboard project is designed specifically
                   for students, offering features like attending classes,
                   submitting assignments, viewing the dashboard, submitting web
@@ -113,11 +102,7 @@ const Login = () => {
               </div>
             </div>
             <div className="modal-footer">
-              <button
-                type="button"
-                className="btn btn-danger"
-                data-bs-dismiss="modal"
-              >
+              <button type="button" className="" data-bs-dismiss="modal">
                 Close
               </button>
             </div>
